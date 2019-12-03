f16e468e45906d70b2cf06b1f1099c92bd681da9
Jiani Fan <jiani.fan@gmail.com>
2019-12-02 16:39:19 -0800

    Create cart reducer

---
+++ frontend/reducers/cart_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_CART  } from "../actions/cart_actions";
+import { RECEIVE_CART_ITEM, REMOVE_CART_ITEM } from "../actions/cart_item_actions";
+
+const cartReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_CART:
+      return action.cart;
+    
+    case RECEIVE_CART_ITEM:
+      newState[action.cartItem.id] = action.cartItem;
+      return newState;
+    
+    case REMOVE_CART_ITEM:
+      delete newState[action.cartItemId];
+      return newState;
+      
+    default:
+      return state;
+  }
+}
+
+export default cartReducer;
