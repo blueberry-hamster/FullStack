5c868c1b3f77707384d84fc431553aa361414ed4
Jiani Fan <jiani.fan@gmail.com>
2019-12-02 16:41:31 -0800

    Update entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,13 +4,15 @@ import productsReducer from './products_reducer';
 import productReducer from './product_reducer';
 import categoriesReducer from './categories_reducer';
 import productCategoriesReducer from './product_categories_reducer';
+import cartReducer from './cart_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   product: productReducer,
   products: productsReducer,
   categories: categoriesReducer,
-  productCategories: productCategoriesReducer
+  productCategories: productCategoriesReducer,
+  cart: cartReducer
 });
 
 export default entitiesReducer;
