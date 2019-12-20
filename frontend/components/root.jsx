226b1811b84f4af84bb13e75562982e550cd1b74
Jiani Fan <jiani.fan@gmail.com>
2019-12-19 19:15:17 -0800

    Move key to key folder

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { ipFindKey } from "../util/keys";
 import App from './app';
 
-$.getJSON("https://ipfind.co/?ip=12.23.56.98&auth=93689cdc-104e-43cb-a5d7-e8470f0580e1", result => {
+$.getJSON(`https://ipfind.co/?ip=12.23.56.98&auth=${ipFindKey}`, result => {
   console.log('res', result);
 });
 
