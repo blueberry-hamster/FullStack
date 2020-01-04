8cd3f020303e84055c50d66701d17468437fae75
Jiani Fan <jiani.fan@gmail.com>
2020-01-03 22:06:14 -0800

    Clear cart on checkout

--- frontend/reducers/cart_reducer.js
+++ frontend/reducers/cart_reducer.js
@@ -9,33 +9,43 @@ const cartReducer = (state = { cartItems: {}, cartId: null}, action) => {
 
   switch (action.type) {
     case RECEIVE_CART:
-      const cartItems = Object.values(action.cart.cartItems);
-      let subtotal = 0;
-      let totalCount = 0;
-
-      const numItems = cartItems.length;
-
-      for (let i = 0; i < numItems; i++) {
-        let item = cartItems[i];
-        let itemPrice = item.product.price;
-
-        subtotal += (item.quantity * itemPrice);
-        totalCount += (item.quantity);
-      }
-      let taxRate = 0.08; //FIXME you can update taxrate based upon user address info
-      let shippingCost = (subtotal === 0) ? 0 : 5.00; //FIXME can change shipping cost based on total in the future
-      // const shippingCost = (subtotal === 0 || subtotal > 30) ? 0 : 5.00; //FIXME can change shipping cost based on total in the future
-      subtotal = subtotal;
-      let totalPrice = (subtotal + (subtotal * taxRate) + shippingCost);
       
-      newState.cartItems = action.cart.cartItems;
-      newState.cartId = action.cart.cartId;
+      if (action.cart.cartItems) {
 
-      newState.shippingCost = shippingCost;
-      newState.subtotal = subtotal;
-      newState.totalPrice = totalPrice;
-      newState.totalCount = totalCount;
-      newState.taxRate = taxRate;
+        const cartItems = Object.values(action.cart.cartItems);
+        let subtotal = 0;
+        let totalCount = 0;
+  
+        const numItems = cartItems.length;
+  
+        for (let i = 0; i < numItems; i++) {
+          let item = cartItems[i];
+          let itemPrice = item.product.price;
+  
+          subtotal += (item.quantity * itemPrice);
+          totalCount += (item.quantity);
+        }
+        let taxRate = 0.08; //FIXME you can update taxrate based upon user address info
+        let shippingCost = (subtotal === 0) ? 0 : 5.00; //FIXME can change shipping cost based on total in the future
+        // const shippingCost = (subtotal === 0 || subtotal > 30) ? 0 : 5.00; //FIXME can change shipping cost based on total in the future
+        subtotal = subtotal;
+        let totalPrice = (subtotal + (subtotal * taxRate) + shippingCost);
+  
+        newState.shippingCost = shippingCost;
+        newState.subtotal = subtotal;
+        newState.totalPrice = totalPrice;
+        newState.totalCount = totalCount;
+        newState.taxRate = taxRate;
+      } else {
+        newState.shippingCost = 0;
+        newState.subtotal = 0;
+        newState.totalPrice = 0;
+        newState.totalCount = 0;
+        newState.taxRate = 0.08;
+      }
+
+      newState.cartItems = action.cart.cartItems || {};
+      newState.cartId = action.cart.cartId;
 
       return newState;
     
